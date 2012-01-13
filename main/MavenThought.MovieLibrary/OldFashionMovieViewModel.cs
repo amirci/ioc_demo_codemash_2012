55de1bda4d77ff41aec5e46f80a9fc0f3252891f
Amir Barylko <amir@barylko.com>
2012-01-13 01:10:18 -0600

    Added old fashion view model and startable folder and example

---
+++ main/MavenThought.MovieLibrary/OldFashionMovieViewModel.cs
@@ -0,0 +1,33 @@
+﻿using System.ComponentModel;
+
+namespace MavenThought.MovieLibrary
+{
+    public class OldFashionMovieViewModel : IMovieViewModel
+    {
+        private string _title;
+
+        private string _description;
+
+        public virtual string Title
+        {
+            get { return _title; }
+            set
+            {
+                _title = value;
+                this.PropertyChanged(this, new PropertyChangedEventArgs("Title"));
+            }
+        }
+
+        public virtual string Description
+        {
+            get { return _description; }
+            set
+            {
+                _description = value;
+                this.PropertyChanged(this, new PropertyChangedEventArgs("Description"));
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged = (s, e) => { };
+    }
+}
---
+++ tests/MavenThought.IoCDemo.Tests/4.Startable/When_registering_startable_type.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using Castle.Core;
+using Castle.Facilities.Startable;
+using Castle.MicroKernel.Registration;
+using MavenThought.Commons.Testing;
+using SharpTestsEx;
+
+namespace MavenThought.IoCDemo.Tests
+{
+    public class CallActionOnStart : IStartable
+    {
+        private readonly Action _action;
+
+        public CallActionOnStart(Action action)
+        {
+            _action = action;
+        }
+
+        public void Start()
+        {
+            _action();
+        }
+
+        public void Stop() {}
+    }
+
+    public class When_registering_startable_type : WindsorContainerSpecification
+    {
+        private bool _wasStarted;
+
+        protected override void GivenIRegister()
+        {
+            this.Sut.Kernel.AddFacility<StartableFacility>();
+
+            Action started = () => this._wasStarted = true;
+
+            this.Sut.Register(
+                Component
+                    .For<CallActionOnStart>()
+                    .DependsOn(new { action = started }));
+        }
+
+        protected override void WhenIRun()
+        {
+            // nothing to run
+        }
+
+        [It]
+        public void Should_be_started()
+        {
+            this._wasStarted.Should().Be.True();
+        }
+    }
+}
